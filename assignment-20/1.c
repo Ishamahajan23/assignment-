7077a58f5169b35c77e4b20d9b8aa4ce326a3f2c
Ishamahajan23 <108974410+Ishamahajan23@users.noreply.github.com>
2022-09-03 20:35:09 +0530

    Create 1.c

---
+++ assignment-20/1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int swap(int*,int*);
+int main(){
+    int a,b;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    swap(&a,&b);
+    printf("a=%d b=%d",a,b);
+
+    return 0;
+}
+int swap(int *p,int *q){
+     int t;
+     t=*p;
+     *p=*q;
+     *q=t;
+
+}
